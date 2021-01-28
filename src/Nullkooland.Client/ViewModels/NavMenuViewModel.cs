29d4f9cd3f2bcf42180192d5592faa1f56145e39
Goose-Bomb <goose_bomb@outlook.com>
2021-01-28 22:36:11 +0800

    Add about page content

--- src/Nullkooland.Client/ViewModels/NavMenuViewModel.cs
+++ src/Nullkooland.Client/ViewModels/NavMenuViewModel.cs
@@ -10,10 +10,10 @@ namespace Nullkooland.Client.ViewModels
             Items = new[]
             {
                 new NavMenuItem {Title = "Home", Icon = Icons.Filled.Home, Url = ""},
-                new NavMenuItem {Title = "Archives", Icon = Icons.Filled.AllInbox, Url = "/archives"},
-                new NavMenuItem {Title = "Tags", Icon = Icons.Filled.Style, Url = "/tags"},
-                new NavMenuItem {Title = "Search", Icon = Icons.Filled.Search, Url = "/search"},
-                new NavMenuItem {Title = "About", Icon = Icons.Filled.TagFaces, Url = "/about"}
+                new NavMenuItem {Title = "Archives", Icon = Icons.Filled.AllInbox, Url = "archives"},
+                new NavMenuItem {Title = "Tags", Icon = Icons.Filled.Style, Url = "tags"},
+                new NavMenuItem {Title = "Search", Icon = Icons.Filled.Search, Url = "search"},
+                new NavMenuItem {Title = "About", Icon = Icons.Filled.TagFaces, Url = "about"}
             };
         }
 
