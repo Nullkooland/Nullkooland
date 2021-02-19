5325d53bd765cbba82a752f1bb504196998ef79f
Goose-Bomb <goose_bomb@outlook.com>
2021-02-19 22:51:20 +0800

    Tweak ListRenderer

--- src/Nullkooland.Client/Services/Markdown/Renderers/ListRenderer.cs
+++ src/Nullkooland.Client/Services/Markdown/Renderers/ListRenderer.cs
@@ -12,19 +12,20 @@ namespace Nullkooland.Client.Services.Markdown.Renderers
             {
                 var item = listBlock[i] as ListItemBlock;
                 renderer.Builder.OpenElement(0, "div");
-                renderer.Builder.AddAttribute(1, "class", "ml-4 d-flex flex-row align-center");
+                renderer.Builder.AddAttribute(1, "class", "ml-4 d-flex flex-row justify-start align-start");
 
 
                 renderer.Builder.OpenComponent<MudText>(2);
-                renderer.Builder.AddAttribute(3, "Style", "font-weight: bolder");
+                renderer.Builder.AddAttribute(3, "Class", "mt-2");
+                renderer.Builder.AddAttribute(4, "Style", "font-weight: bolder");
 
                 string bullet = GetBullet(listBlock.BulletType, i);
-                renderer.Builder.AddAttribute(4, "ChildContent",
-                    (RenderFragment) (builder => builder.AddMarkupContent(5, bullet))
+                renderer.Builder.AddAttribute(5, "ChildContent",
+                    (RenderFragment) (builder => builder.AddMarkupContent(6, bullet))
                 );
 
 
-                renderer.Builder.AddAttribute(9, "Color", Color.Primary);
+                renderer.Builder.AddAttribute(7, "Color", Color.Primary);
                 renderer.Builder.CloseComponent();
 
                 renderer.Write(item[0]);
