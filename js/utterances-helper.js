479c97b97f3d42532fbe56dcf696ee1c6c075be1
Goose-Bomb <Goose-Bomb@users.noreply.github.com>
2021-11-22 04:44:04 +0000

    Deploying to gh-pages from @ Goose-Bomb/Nullkooland@7230c20ef1e597ba15ecfdef270abd316de91d70 🚀

---
+++ js/utterances-helper.js
@@ -0,0 +1,13 @@
+﻿window.utterancesHelper = {
+    render: function (element) {
+        let utterances = document.createElement("script");
+        utterances.type = "text/javascript";
+        utterances.src = "https://utteranc.es/client.js";
+        utterances.async = true;
+        utterances.crossorigin = "anonymous";
+        utterances.setAttribute("issue-term", "pathname")
+        utterances.setAttribute("theme", "preferred-color-scheme")
+        utterances.setAttribute("repo", "Goose-Bomb/Nullkooland")
+        element.appendChild(utterances);
+    }
+};
