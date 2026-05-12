275c64110e1b55e364e7f158479096f6de34a22c
Nullkooland <Nullkooland@users.noreply.github.com>
2026-05-12 18:12:32 +0000

    Deploying to gh-pages from @ Nullkooland/Nullkooland@ad24d95ed10cb6ed33d115a58412fe1c4e20d605 🚀

--- js/utterances-helper.js
+++ js/utterances-helper.js
@@ -7,7 +7,7 @@
         utterances.crossorigin = "anonymous";
         utterances.setAttribute("issue-term", "pathname")
         utterances.setAttribute("theme", "preferred-color-scheme")
-        utterances.setAttribute("repo", "Goose-Bomb/Nullkooland")
+        utterances.setAttribute("repo", "Nullkooland/Nullkooland")
         element.appendChild(utterances);
     }
 };
